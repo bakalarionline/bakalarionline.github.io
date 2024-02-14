ea569a7c66a2b3132455cc296a5dfb1a2d6dbbfa
bakalarionline <160078352+bakalarionline@users.noreply.github.com>
2024-02-14 23:43:42 +0100

    Add files via upload

---
+++ Bakalari - login to the system_files/loginFunctions.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+    if (screen.width < 640) {
+        var element = $('#bkViewport');
+        element.attr('content', 'width=500');
+    }
+
+    ls_capslock.init({
+        element: '#password',
+        message: 'Máte zapnutý Caps Lock',
+        position: 'bottom'
+    });
+});
+
+/** 
+ *  Volá se při odeslání formu přihlášení. Zamění text v přihlašovacím tlačítku za loading a znepřístupní ho,
+ * dohledá tlačítka pro LiveId a MS a případně je nastaví na disabled. 
+ * */
+function makeDisable() {
+    $("#loginButton .btn-login-text").hide();
+    $("#loginButton .btn-login-loader").show();
+    document.getElementById("loginButton").disabled = true;
+    const liveIdBtn = document.getElementById("liveidButton");
+    if (liveIdBtn) {
+        liveIdBtn.disabled = true;
+    }
+    const msLoginBtn = document.getElementById("loginMSButton");
+    if (msLoginBtn) {
+        msLoginBtn.disabled = true;
+    }
+}
+
+/**
+ * Akce po stisknutí klávesy enter
+ * @param {any} event Informace o události
+ */
+function onEnterSubmit(event) {
+    if (event.keyCode == 13) {
+        $("#loginButton").click();
+    }
+}
---
+++ Bakalari - login to the system_files/master_core.js
@@ -0,0 +1,105 @@
+﻿$(
+    function () {
+        CheckUnsupportedBrowser();
+        CheckConnection();
+        CheckNetCoreWeb();
+    }
+)
+
+/**
+ * Otevření popup okna
+ * @param {any} element prvek nad kterým se má popup zobrazit
+ * @param {any} position pozice popupu
+ */
+function showPopup(element, position) {
+    var id = element.id;
+    var popup = "#" + id + "_popUp";
+    if (!position) {
+        position = { my: "center", at: "center", of: window, collision: 'fit' };
+    }
+    $(popup).dialog({
+        dialogClass: "no-titlebar",
+        clickOutside: true,
+        clickOutsideTrigger: "#" + id,
+        position: position,
+        draggable: false,
+        resizable: false,
+        show: {
+            effect: "fade",
+            duration: 100
+        },
+        hide: {
+            effect: "fade",
+            duration: 100
+        }
+    });
+}
+//Pro odstranění focusu na close button
+$.ui.dialog.prototype._focusTabbable = $.noop;
+//Doplněk pro zavírání jquery dialogu po kliknutí mimo něj
+$.widget('ui.dialog', $.ui.dialog, {
+    options: {
+        clickOutside: false,
+        clickOutsideTrigger: ''
+    },
+    open: function () {
+        var clickOutsideTriggerEl = $(this.options.clickOutsideTrigger),
+            that = this;
+        if (this.options.clickOutside) {
+            $(document).on('click.ui.dialogClickOutside' + that.eventNamespace, function (event) {
+                var $target = $(event.target);
+                if ($target.closest($(clickOutsideTriggerEl)).length === 0 &&
+                    $target.closest($(that.uiDialog)).length === 0) {
+                    that.close();
+                }
+            });
+        }
+        this._super();
+    },
+    close: function () {
+        $(document).off('click.ui.dialogClickOutside' + this.eventNamespace);
+        this._super();
+    },
+});
+
+/**
+ * Zobrazení panelu s obecným varováním pro uživatele
+ * @param {any} message zpráva obsahu
+ */
+function ShowGeneralWarningPanel(message) {
+    /**Panel pro obecné varování zobrazené v hlavním layoutu */
+    var warningPanel = $("#generalWarning");
+    warningPanel.addClass("visible"); //přidaná třída 'visible' kvůli selectoru v css
+    warningPanel.empty();
+    warningPanel.html(message);
+    warningPanel.show();
+}
+
+/**Kontrola podporovaného prohlížeče kvůli ukončení podpory IE */
+function CheckUnsupportedBrowser() {
+    if (!(/(Chrome|Safari|Firefox)/.test(navigator.userAgent))) {
+        ShowGeneralWarningPanel(unsupportedBrowserMsg);
+    }
+}
+
+/**Detekce nezabezpečeného spojení */
+function CheckConnection() {
+    if (window.location.protocol == 'http:') {
+        /**Testovací url pro https */
+        var url = 'https://' + window.location.host + window.location.pathname;
+        $.get(url)
+            .done(function () {
+                window.location.href.replace('http:', 'https:');
+            })
+            .fail(function () {
+                ShowGeneralWarningPanel(unsecuredConnectionMsg);
+            });
+    }
+}
+
+/** Kontrola na běh NetCore webovky */
+const CheckNetCoreWeb = () => {
+    if (typeof isNetCoreRunning !== "undefined" && !isNetCoreRunning) {
+        ShowGeneralWarningPanel(netCoreIsNotRunningMsg);
+    }
+}
